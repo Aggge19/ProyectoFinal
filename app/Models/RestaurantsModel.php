f7dffef19c966d824ab00ce281590418ad716c53
Aggge19 <angelagcre19@gmail.com>
2022-01-25 16:40:54 +0100

    modelos hechos

---
+++ app/Models/RestaurantsModel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Database\Migrations\Restaurants;
+use CodeIgniter\Model;
+
+class RestaurantsModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'restaurants';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = Restaurants::class;
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['name','description','address','latitude','longitude','reviewAverage','numReviews'];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
